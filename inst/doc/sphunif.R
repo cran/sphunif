a35c4e9e6cf0b145391284e64ea6bd794caaadf9
Eduardo García-Portugués <edgarcia@est-econ.uc3m.es>
2023-12-04 19:50:02 +0000

    version 1.3.0

--- inst/doc/sphunif.R
+++ inst/doc/sphunif.R
@@ -5,47 +5,47 @@ knitr::opts_chunk$set(
   comment = "#>"
 )
 
-## ---- cir_data----------------------------------------------------------------
+## ----cir_data-----------------------------------------------------------------
 set.seed(987202226)
 cir_data <- runif(n = 30, min = 0, max = 2 * pi)
 
-## ---- unif_test_cir-----------------------------------------------------------
+## ----unif_test_cir------------------------------------------------------------
 library(sphunif)
 unif_test(data = cir_data, type = "Watson") # An htest object
 
-## ---- asymp-------------------------------------------------------------------
+## ----asymp--------------------------------------------------------------------
 unif_test(data = cir_data, type = "Watson", p_value = "MC") # Monte Carlo
 unif_test(data = cir_data, type = "Watson", p_value = "asymp") # Asymp. distr.
 
-## ---- avail_cir---------------------------------------------------------------
+## ----avail_cir----------------------------------------------------------------
 avail_cir_tests
 
-## ---- unif_test_avail_cir-----------------------------------------------------
+## ----unif_test_avail_cir------------------------------------------------------
 # A *list* of htest objects
 unif_test(data = cir_data, type = c("PAD", "Watson"))
 
-## ---- sph_data----------------------------------------------------------------
+## ----sph_data-----------------------------------------------------------------
 # Sample data on S^2
 set.seed(987202226)
 theta <- runif(n = 30, min = 0, max = 2 * pi)
 phi <- runif(n = 30, min = 0, max = pi)
 sph_data <- cbind(cos(theta) * sin(phi), sin(theta) * sin(phi), cos(phi))
 
-## ---- avail_sph---------------------------------------------------------------
+## ----avail_sph----------------------------------------------------------------
 avail_sph_tests
 
-## ---- unif_test_avail_sph-----------------------------------------------------
+## ----unif_test_avail_sph------------------------------------------------------
 unif_test(data = sph_data, type = "all", p_value = "MC", M = 1e3)
 unif_test(data = sph_data, type = "Rayleigh", p_value = "asymp")
 
-## ---- hyp_data----------------------------------------------------------------
+## ----hyp_data-----------------------------------------------------------------
 # Sample data on S^9
 hyp_data <- r_unif_sph(n = 30, p = 10)
 
 # Test
 unif_test(data = hyp_data, type = "Rayleigh", p_value = "asymp")
 
-## ---- venus-------------------------------------------------------------------
+## ----venus--------------------------------------------------------------------
 # Load spherical data
 data(venus)
 head(venus)
@@ -73,14 +73,14 @@ with_progress(
             p_value = "MC", chunks = 1e2, M = 5e2, cores = 2)
 )
 
-## ---- unif_stat_vec-----------------------------------------------------------
+## ----unif_stat_vec------------------------------------------------------------
 # M samples of size n on S^2
 samps_sph <- r_unif_sph(n = 30, p = 3, M = 10)
 
 # Apply all statistics to the M samples
 unif_stat(data = samps_sph, type = "all")
 
-## ---- MC----------------------------------------------------------------------
+## ----MC-----------------------------------------------------------------------
 # Break the simulation in 10 chunks of tasks to be divided between 2 cores
 sim <- unif_stat_MC(n = 30, type = "all", p = 3, M = 1e4, cores = 2,
                     chunks = 10)
@@ -115,7 +115,7 @@ pow <- unif_stat_MC(n = 30, type = "all", p = 3, M = 1e2, cores = 2,
                     chunks = 5, r_H1 = r_H1, crit_val = sim$crit_val_MC)
 pow$power_MC
 
-## ---- crit_val----------------------------------------------------------------
+## ----crit_val-----------------------------------------------------------------
 # Using precomputed critical values
 ht1 <- unif_test(data = samps_sph[, , 1], type = "Rayleigh",
                  p_value = "crit_val", crit_val = sim$crit_val_MC)
